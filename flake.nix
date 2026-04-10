4b16f582806ec821d7664a50c9472b03123599b5
Louis Gérard <louis.gerard.pro@proton.me>
2026-04-10 11:08:27 +0200

    try with flake
    Signed-off-by: Louis Gérard <louis.gerard.pro@proton.me>

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "Development environments for this project";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = {nixpkgs, ...}: let
+    systems = [
+      "x86_64-linux"
+      "aarch64-darwin"
+    ];
+  in {
+    devShells = builtins.listToAttrs (
+      builtins.map (system:  let 
+        pkgs = import nixpkgs { inherit system; };
+        shell = pkgs.mkShell {
+          packages = [ 
+            pkgs.go
+            pkgs.golangci-lint
+          ];
+        };
+      in {
+        name = system;
+        value =  { 
+          default = shell;
+        };
+      }) systems
+    );
+  };
+}
